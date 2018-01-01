81a797456205f9bd403a85d19581169008be6f96
Ayush Jain <ayush@culturealley.com>
2018-01-01 21:53:56 +0530

    new year resolution added

---
+++ app/src/main/java/com/instadownloader/instadpdownloader/Utils/NewYear2018.java
@@ -0,0 +1,14 @@
+package com.instadownloader.instadpdownloader.Utils;
+
+/**
+ * Created by ayush on 1/1/18.
+ */
+
+//TODO Never be cruel, never be cowardly and never, ever eat pears!
+//TODO Remember... hate is always foolish... and love is always wise.
+//TODO Always try to be nice, but never fail to be kind.
+//TODO Laugh hard...run fast...be kind.
+
+public class NewYear2018 {
+
+}
